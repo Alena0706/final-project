103496606b0d8466aa72d4b2ccf1d0de63b8d06d
1babii1 <sabangulov.ilnaz@mail.ru>
2025-09-05 11:31:20 +0300

    signin

--- client/src/app/router/AppRouter.tsx
+++ client/src/app/router/AppRouter.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import Layout from '../Layout';
 import SignUpPage from '@/pages/SignUp/ui/SignUpPage';
+import SignInPage from '@/pages/SignIn/ui/SignInPage';
 
 export default function AppRouter(): React.JSX.Element {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/signin" element={<div>SignIn</div>} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Route>
     </Routes>
