084caf6ebb859263a9c93412d71b6cb83118204a
Alena <kirlena95@gmail.com>
2025-09-12 12:28:26 +0300

    fix bugs again

--- server/db/database.js
+++ server/db/database.js
@@ -21,14 +21,12 @@ module.exports = {
     seederStorage: 'sequelize',
     seederStorageTableName: 'SequelizeData',
   },
-  production: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'postgres123',
-    database: process.env.DB_NAME || 'franchise_db',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
+    production: {
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASS_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
-    seederStorageTableName: 'SequelizeData',
+    use_env_variable: process.env.DB_URL_PROD ? 'DB_URL_PROD' : undefined,
   },
 };
